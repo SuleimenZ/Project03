d3afda61c9f06040c8c824e9d9caf8d8e34df8e2
SuleimenZ <62009948+SuleimenZ@users.noreply.github.com>
2020-06-02 10:19:38 +0200

    Add project files.

---
+++ Project03/ArrayGenerator.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Projec03
+{
+    class ArrayGenerator
+    {
+
+        public static int[] Generate(int size)
+        {
+            var rnd = new Random();
+            var array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(int.MinValue, int.MaxValue);
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            var rnd = new Random();
+            var array = new int[1000000];
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                array[i] = rnd.Next(max + 1);
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateSorted(int size)
+        {
+            var array = Generate(size);
+            Array.Sort(array);
+
+            return array;
+        }
+
+        public static int[] GenerateSortedSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            var array = GenerateSpecific(max);
+            Array.Sort(array);
+
+            return array;
+        }
+
+
+        public static int[] GenerateReversed(int size)
+        {
+            var array = GenerateSorted(size);
+            Array.Reverse(array);
+
+            return array;
+        }
+
+        public static int[] GenerateReversedSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            var array = GenerateSortedSpecific(max);
+            Array.Reverse(array);
+
+            return array;
+        }
+
+        public static int[] GenerateConstant(int size)
+        {
+            var rnd = new Random();
+            var array = new int[size];
+
+            var value = rnd.Next(int.MinValue, int.MaxValue);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateConstantSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            var rnd = new Random();
+            var array = new int[1000000];
+
+            var value = rnd.Next(max + 1);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateAShaped(int size)
+        {
+            int index = 1;
+            int first = 1; // index 0 is always at 0 so we start from 1;
+            int last = size - 1;
+
+            var array = GenerateSorted(size);
+            var temparray = new int[size];
+
+            array.CopyTo(temparray, 0);
+
+            while (first <= last)
+            {
+                if (index % 2 == 1)
+                {
+                    array[last--] = temparray[index++];
+                }
+                else
+                {
+                    array[first++] = temparray[index++];
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateAShapedSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            int index = 1;
+            int first = 1; // index 0 is always at 0 so we start from 1;
+            int last = 1000000 - 1;
+
+            var array = GenerateSortedSpecific(max);
+            var temparray = new int[1000000];
+
+            array.CopyTo(temparray, 0);
+
+            while (first <= last)
+            {
+                if (index % 2 == 1)
+                {
+                    array[last--] = temparray[index++];
+                }
+                else
+                {
+                    array[first++] = temparray[index++];
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateVShaped(int size)
+        {
+            int index = (size - 1) / 2; // center
+            int tempindex = 0;
+
+            var array = GenerateSorted(size);
+            var temparray = new int[size];
+
+            array.CopyTo(temparray, 0);
+
+            for (int i = 1; tempindex != size; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    array[index] = temparray[tempindex++];
+                    index += i;
+                }
+                else
+                {
+                    array[index] = temparray[tempindex++];
+                    index -= i;
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateVShapedSpecific(int max) // values from 0 to max, size 1.000.000
+        {
+            int index = (1000000 - 1) / 2; // center
+            int tempindex = 0;
+
+            var array = GenerateSortedSpecific(max);
+            var temparray = new int[1000000];
+
+            array.CopyTo(temparray, 0);
+
+            for (int i = 1; tempindex != 1000000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    array[index] = temparray[tempindex++];
+                    index += i;
+                }
+                else
+                {
+                    array[index] = temparray[tempindex++];
+                    index -= i;
+                }
+            }
+
+            return array;
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            int length = array.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                int index = i;
+
+                for (int j = i + 1; j < length; j++) // from unsorted to the end
+                {
+                    if (array[j] < array[index])
+                    {
+                        index = j;
+                    }
+                }
+
+                int temp = array[index];
+                array[index] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            int tempvalue;
+            bool swapped;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        tempvalue = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tempvalue;
+                        swapped = true;
+                    }
+                }
+
+                if (swapped == false)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void HeapSort(int[] array)
+        {
+            int length = array.Length;
+
+            for (int i = length / 2 - 1; i >= 0; i--)
+            {
+                MakeHeap(array, length, i);
+            }
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                int temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+
+                MakeHeap(array, i, 0);
+            }
+        }
+
+        private static void MakeHeap(int[] array, int length, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1; // left child
+            int right = 2 * i + 2; // right child
+            int temp;
+
+            if (left < length && array[left] > array[largest])
+            {
+                largest = left;
+            }
+
+            if (right < length && array[right] > array[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                temp = array[i];
+                array[i] = array[largest];
+                array[largest] = temp;
+
+                MakeHeap(array, length, largest);
+            }
+        }
+
+
+        public static void CountingSort(int[] array, int max)
+        {
+            var count = new int[max + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                ++count[array[i]];
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                for (int j = 0; j < count[i]; j++)
+                {
+                    array[index++] = i;
+                }
+            }
+        }
+
+        private static int FindPivot(int[] array, int start, int end)
+        {
+            int pivot = array[start];
+            while (true)
+            {
+
+                while (array[start] < pivot)
+                {
+                    start++;
+                }
+
+                while (array[end] > pivot)
+                {
+                    end--;
+                }
+
+                if (start < end)
+                {
+                    if (array[start] == array[end])
+                    {
+                        return end;
+                    }
+
+                    int temp = array[start];
+                    array[start] = array[end];
+                    array[end] = temp;
+
+                }
+                else
+                {
+                    return end;
+                }
+            }
+        }
+    }
+}
---
+++ Project03/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projec03
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int size;
+            bool isInt = false;
+            char userInput;
+            int[] randomArray, ascendingArray, reversedArray, vShapedArray, aShapedArray;
+
+            var stopwatch = new Stopwatch();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Choose:\n'1' for Bubble Sort;\n'2' for Selection Sort;\n'3' for Heap Sort;\n'4' for Counting sort(array size = 1.000.000);");
+                userInput = Console.ReadKey().KeyChar;
+
+            } while (!(userInput == '1' || userInput == '2' || userInput == '3' || userInput == '4'));
+            Console.Clear();
+
+            switch (userInput)
+            {
+                case '1':
+                case '2':
+                case '3':
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("What size of array should be?");
+                        isInt = int.TryParse(Console.ReadLine(), out size);
+                    } while (!isInt || !(size > 0) || !(size < int.MaxValue));
+                    Console.Clear();
+
+                    randomArray = ArrayGenerator.Generate(size);
+                    ascendingArray = ArrayGenerator.GenerateSorted(size);
+                    reversedArray = ArrayGenerator.GenerateReversed(size);
+                    vShapedArray = ArrayGenerator.GenerateVShaped(size);
+                    aShapedArray = ArrayGenerator.GenerateAShaped(size);
+
+                    switch (userInput)
+                    {
+                        case '1':
+                            stopwatch.Start();
+                            ArrayGenerator.BubbleSort(randomArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"randomArray:      {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.BubbleSort(ascendingArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"ascendingArray:   {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.BubbleSort(reversedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"reversedArray:    {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.BubbleSort(vShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"vShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.BubbleSort(aShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"aShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            break;
+                        case '2':
+                            stopwatch.Start();
+                            ArrayGenerator.SelectionSort(randomArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"randomArray:      {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.SelectionSort(ascendingArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"ascendingArray:   {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.SelectionSort(reversedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"reversedArray:    {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.SelectionSort(vShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"vShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.SelectionSort(aShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"aShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            break;
+                        case '3':
+                            stopwatch.Start();
+                            ArrayGenerator.HeapSort(randomArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"randomArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.HeapSort(ascendingArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"ascendingArray:  {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.HeapSort(reversedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"reversedArray:   {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.HeapSort(vShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"vShapedArray:    {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            stopwatch.Start();
+                            ArrayGenerator.HeapSort(aShapedArray);
+                            stopwatch.Stop();
+                            Console.WriteLine($"aShapedArray:    {stopwatch.Elapsed.TotalMilliseconds}ms");
+                            stopwatch.Reset();
+
+                            break;
+                    }
+                    break;
+                case '4':
+                    int max;
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("What maximum int value should be?(values: from 0 to max)");
+                        isInt = int.TryParse(Console.ReadLine(), out max);
+                    } while (!isInt || !(max > 0) || !(max < int.MaxValue - 1));
+                    Console.Clear();
+
+                    randomArray = ArrayGenerator.GenerateSpecific(max);
+                    ascendingArray = ArrayGenerator.GenerateSortedSpecific(max);
+                    reversedArray = ArrayGenerator.GenerateReversedSpecific(max);
+                    vShapedArray = ArrayGenerator.GenerateVShapedSpecific(max);
+                    aShapedArray = ArrayGenerator.GenerateAShapedSpecific(max);
+
+                    stopwatch.Start();
+                    ArrayGenerator.CountingSort(randomArray, max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"randomArray:      {stopwatch.Elapsed.TotalMilliseconds}ms");
+                    stopwatch.Reset();
+
+                    stopwatch.Start();
+                    ArrayGenerator.CountingSort(ascendingArray, max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"ascendingArray:   {stopwatch.Elapsed.TotalMilliseconds}ms");
+                    stopwatch.Reset();
+
+                    stopwatch.Start();
+                    ArrayGenerator.CountingSort(reversedArray, max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"reversedArray:    {stopwatch.Elapsed.TotalMilliseconds}ms");
+                    stopwatch.Reset();
+
+                    stopwatch.Start();
+                    ArrayGenerator.CountingSort(vShapedArray, max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"vShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                    stopwatch.Reset();
+
+                    stopwatch.Start();
+                    ArrayGenerator.CountingSort(aShapedArray, max);
+                    stopwatch.Stop();
+                    Console.WriteLine($"aShapedArray:     {stopwatch.Elapsed.TotalMilliseconds}ms");
+                    stopwatch.Reset();
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
